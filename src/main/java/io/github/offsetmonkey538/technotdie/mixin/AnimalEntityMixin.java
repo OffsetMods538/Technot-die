7f5bb0709a037ea0fe47754981e7473e7979fd36
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 21:07:38 +0300

    Inject into the `damage` method of AnimalEntity

--- src/main/java/io/github/offsetmonkey538/technotdie/mixin/AnimalEntityMixin.java
+++ src/main/java/io/github/offsetmonkey538/technotdie/mixin/AnimalEntityMixin.java
@@ -1,8 +1,17 @@
 package io.github.offsetmonkey538.technotdie.mixin;
 
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.AnimalEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AnimalEntity.class)
 public class AnimalEntityMixin {
+
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+
+    }
 }
