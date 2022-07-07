0b5f272ac76019368908462507131533a9c26dcc
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 21:11:52 +0300

    Check if the entity is a pig named "Technoblade" and return from the method if it is.

--- src/main/java/io/github/offsetmonkey538/technotdie/mixin/AnimalEntityMixin.java
+++ src/main/java/io/github/offsetmonkey538/technotdie/mixin/AnimalEntityMixin.java
@@ -7,11 +7,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static io.github.offsetmonkey538.technotdie.TechnotDie.isTechnoPig;
+
 @Mixin(AnimalEntity.class)
 public class AnimalEntityMixin {
 
     @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
     public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-
+        if (isTechnoPig(this)) {
+            cir.setReturnValue(false);
+        }
     }
 }
