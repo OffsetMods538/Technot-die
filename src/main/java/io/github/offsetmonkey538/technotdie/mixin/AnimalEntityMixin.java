357314bce78b765aacce0ebcf8a327abf5ae74ed
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-07 21:03:20 +0300

    Add mixin into AnimalEntity

---
+++ src/main/java/io/github/offsetmonkey538/technotdie/mixin/AnimalEntityMixin.java
@@ -0,0 +1,8 @@
+package io.github.offsetmonkey538.technotdie.mixin;
+
+import net.minecraft.entity.passive.AnimalEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AnimalEntity.class)
+public class AnimalEntityMixin {
+}
