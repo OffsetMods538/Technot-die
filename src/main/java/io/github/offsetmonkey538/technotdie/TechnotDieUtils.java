ffec8fe2250d05ea8787cbdd830405879fbe7baf
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-27 22:28:08 +0300

    Make the code easier to understand

--- src/main/java/io/github/offsetmonkey538/technotdie/TechnotDieUtils.java
+++ src/main/java/io/github/offsetmonkey538/technotdie/TechnotDieUtils.java
@@ -2,13 +2,15 @@ package io.github.offsetmonkey538.technotdie;
 
 
 import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.text.Text;
 
 public class TechnotDieUtils {
 
     public static boolean isTechnoPig(Object entity) {
         if (entity instanceof PigEntity) {
             PigEntity pig = (PigEntity) entity;
-            return pig.getCustomName() != null && pig.getCustomName().getString().equals("Technoblade");
+            Text name = pig.getCustomName();
+            return name != null && name.getString().equals("Technoblade");
         }
         return false;
     }
